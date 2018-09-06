251bfa6d2b3dd66e3cf3749b1cc87e66e451bf42
VadimZverev <39337335+VadimZverev@users.noreply.github.com>
2018-09-06 12:27:21 +0400

    Создаем легко связываемый HTML для визуализации свыойств из другого объекта модели посредством связывания

--- MvcModels/Controllers/HomeController.cs
+++ MvcModels/Controllers/HomeController.cs
@@ -11,41 +11,11 @@ namespace MvcModels.Controllers
     {
         private Person[] personData =
         {
-            new Person
-            {
-                PersonId = 1,
-                FirstName = "Adam",
-                LastName = "Freeman",
-                Role = Role.Admin
-            },
-            new Person
-            {
-                PersonId = 2,
-                FirstName = "Steven",
-                LastName = "Sanderson",
-                Role = Role.Admin
-            },
-            new Person
-            {
-                PersonId = 3,
-                FirstName = "Jacqui",
-                LastName = "Griffyth",
-                Role = Role.User
-            },
-            new Person
-            {
-                PersonId = 4,
-                FirstName = "John",
-                LastName = "Smith",
-                Role = Role.User
-            },
-            new Person
-            {
-                PersonId = 5,
-                FirstName = "Anne",
-                LastName = "Jones",
-                Role = Role.Guest
-            },
+            new Person { PersonId = 1, FirstName = "Adam", LastName = "Freeman", Role = Role.Admin },
+            new Person { PersonId = 2, FirstName = "Steven", LastName = "Sanderson", Role = Role.Admin },
+            new Person { PersonId = 3, FirstName = "Jacqui", LastName = "Griffyth", Role = Role.User },
+            new Person { PersonId = 4, FirstName = "John", LastName = "Smith", Role = Role.User },
+            new Person { PersonId = 5, FirstName = "Anne", LastName = "Jones", Role = Role.Guest }
         };
 
         public ActionResult Index(int? id = 1)
